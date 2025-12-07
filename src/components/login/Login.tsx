f732976282c3dad8f3aa91fe1df04449be1dfe25
Kauã Santos <kaua.simplicio@fecaf.com.br>
2025-12-07 19:09:42 -0300

    feat: cria os schemas para validação dos dados com o Zod

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,11 +1,38 @@
+"use client";
+
 import Image from "next/image";
 import LoginImage from "../../../public/login-grocey-project.svg";
 import { HiOutlineEye, HiOutlineEyeOff } from "react-icons/hi";
 import { MdOutlineMailOutline } from "react-icons/md";
 import { MdOutlineLock } from "react-icons/md";
 import Link from "next/link";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const loginSchema = z.object({
+  email: z.email("Invalid email format"),
+  password: z
+    .string()
+    .min(6, "Password too short")
+    .max(20, "Password must not exceed 20 characters."),
+});
+
+type LoginInputs = z.infer<typeof loginSchema>;
 
 const Login = () => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<LoginInputs>({
+    resolver: zodResolver(loginSchema),
+  });
+
+  const onSubmit = (data: LoginInputs) => {
+    console.log(data);
+  };
+
   return (
     <section className="bg-[#E8E3CE] min-h-screen">
       <Image
@@ -19,28 +46,49 @@ const Login = () => {
           <p className="text-[#C05100]">Ready to make some shopping?</p>
         </div>
 
-        <form className="mt-5 flex flex-col gap-2">
+        <form
+          className="mt-5 flex flex-col gap-2"
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <label className="text-lg">Email</label>
           <div className="relative flex items-center border border-[#E96301] rounded p-1 text-black">
-            <input type="email" className="mx-7 w-96 outline-none" />
+            <input
+              type="email"
+              className="mx-7 w-96 outline-none"
+              {...register("email")}
+            />
             <MdOutlineMailOutline className="absolute left-2 text-lg text-[#E96301]" />
           </div>
+          {errors.email && (
+            <p className="text-red-500">{errors.email.message}</p>
+          )}
+
           <label className="text-lg">Password</label>
           <div className="relative flex items-center border border-[#E96301] rounded p-1 text-black">
-            <input type="password" className="mx-7 w-96 outline-none" />
+            <input
+              type="password"
+              className="mx-7 w-96 outline-none"
+              {...register("password")}
+            />
             <MdOutlineLock className="absolute left-2 text-lg text-[#E96301]" />
             <HiOutlineEye className="absolute right-2 text-lg text-[#E96301] cursor-pointer" />
           </div>
+          {errors.password && (
+            <p className="text-red-500">{errors.password.message}</p>
+          )}
 
-          <div className="flex justify-between items-center">
+          <div className="flex justify-between items-center mt-1">
             <div className="flex items-center">
-              <input type="checkbox" />
+              <input type="checkbox" className="accent-[#E96301] w-4 h-4" />
               <p className="ml-1">Remember-me</p>
             </div>
-            <Link href={"forget-password"}>forget password?</Link>
+            <Link href={"/forget-password"}>forgot your password?</Link>
           </div>
 
-          <button className="bg-[#E96301] p-2 text-white cursor-pointer mt-4 font-semibold rounded-lg">
+          <button
+            className="bg-[#E96301] p-2 text-white cursor-pointer mt-4 font-semibold rounded-lg"
+            type="submit"
+          >
             Login
           </button>
         </form>
